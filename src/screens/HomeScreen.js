cea3c5f18391b60648a597a473cae161db9b722b
gmarvis <sgmarvis@gmail.com>
2024-05-03 00:27:49 +0100

    welcome splash screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+    return (
+        <View className="bg-slate-300 flex-1 justify-center items-center">
+            <Text className="text-red-600 bg-white py-2 px-4 rounded-full">
+                HomeScreen
+            </Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
